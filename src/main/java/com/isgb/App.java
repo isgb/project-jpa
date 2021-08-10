b27ef9d04d4b953d0c846ee8905e095ce9a0a6ef
isgb <isgb_codigo@hotmail.com>
2021-08-10 10:02:07 -0500

    Version inicial del proyecto project-jpa

---
+++ src/main/java/com/isgb/App.java
@@ -0,0 +1,13 @@
+package com.isgb;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
